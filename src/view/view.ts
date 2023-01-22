f7345f2196bbda620e0712f0aff38cf40e1f5d8a
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-22 18:28:40 -0300

    feat: add escape when someone tries to add a <script> tag to our html

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,7 +1,5 @@
 //Creating a generic type to be defined on the children
 
-import { Negotiation } from "../model/negotiation";
-
 //Implementing an abstract class which means that the parent class doesn't know how it will be implemented, it's a resonsability of the children class
 export abstract class View<T, K> {
   //Used the protected modifier to give access only to children classes
@@ -26,7 +24,11 @@ export abstract class View<T, K> {
   //Implementing an abstract method meaning that the children have to implement a template method for the code to compile
   protected abstract template(model: T, negotiation?: K): string;
   update(model: T, type?: K): void {
-    const template = this.template(model, type);
+    let template = this.template(model, type);
+
+    if (this.escape) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
     this._selectorElement.innerHTML = template;
   }
 }
