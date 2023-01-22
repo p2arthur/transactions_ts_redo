628ddf94392265d45b69bf0dd02e6d221d65163f
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-22 18:41:05 -0300

    refactor: clean unused imports

--- src/view/message-view.ts
+++ src/view/message-view.ts
@@ -1,4 +1,3 @@
-import { Negotiation } from "../model/negotiation.js";
 import { View } from "./view.js";
 //Using generics to pass the type that will enter into the parent View <string> - (Defining the T)
 export class MessageView extends View<string, string> {
